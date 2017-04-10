b0037c99cb28588df80fe5362d6fb2e34b6a0ea5
Emilia Alcocer <emilia.alcocer@irstrat.com>
2017-04-10 11:55:23 -0500

    cambio ==

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ b="Antonio"
 x=10
 y=5
 puts a + " tiene " + x.to_s + " y " + b +" tiene " + y.to_s
-if x+y=15
+if x+y==15
   puts "y juntos tienen 15"
 end
 m={"fabiola" => 6, "miguel" => 23, "antonio" => x, "emilia" => y, "hernan" => 26}
