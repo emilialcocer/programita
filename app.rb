15078f47f57d820ecfe43f8a9def4598a681d846
Emilia Alcocer <emilia.alcocer@irstrat.com>
2017-04-10 11:30:59 -0500

    usando hash

--- app.rb
+++ app.rb
@@ -6,3 +6,6 @@ puts a + " tiene " + x.to_s + " y " + b +" tiene " + y.to_s
 if x+y=15
   puts "y juntos tienen 15"
 end
+m={"fabiola" => 6, "miguel" => 23, "antonio" => x, "emilia" => y, "hernan" => 26}
+puts m["antonio"]
+puts m["emilia"]
