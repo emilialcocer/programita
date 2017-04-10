383130387a5c5c03aa22fe9882c0abf5c01e8bd2
Emilia Alcocer <emilia.alcocer@irstrat.com>
2017-04-10 18:20:11 -0500

    iteración

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ puts a + " tiene " + x.to_s + " y " + b +" tiene " + y.to_s
 if x+y==15
   puts "y juntos tienen 15"
 end
-m={"fabiola" => 6, "miguel" => 23, "antonio" => x, "emilia" => y, "hernan" => 26}
+m={"fabiola" => 25, "miguel" => 20, "antonio" => x, "emilia" => y, "hernan" => 30}
 puts m["antonio"]
 puts m["emilia"]
+m.each{|p,q|puts"#{p}:#{q}"}
+m=30
+loop do
+  m-=5
+  print "#{m}"
+  break if m<=5
+end
